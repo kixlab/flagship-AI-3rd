8f7f83e157870f00ef9a994fb29093ed62ba17f8
Seongho Park <bluetosh7@gmail.com>
2018-07-04 15:09:13 +0900

    fix: tokenize with small iteration

--- bin/tokenize_scripts.py
+++ bin/tokenize_scripts.py
@@ -1,23 +1,24 @@
-from os import listdir, remove, makedirs
+from os import listdir, remove, makedirs, rename
 from os.path import isfile, join, dirname, exists, basename
 import re
 import json
 import zipfile
+import shutil
 
 ## Varaibles
-# All of directories must exist
-read_zip_file = '../scripts/test2.zip'
-write_zip_file = '../results/test2.zip'
-# read_directory = '../scripts'
-write_directory = '.'  # For temp files to be zipped
+# All of directories must be in advance
+read_zip_file = '../scripts/scripts.zip'
+write_zip_file = '../results/result.zip'
+write_directory = '../results/scripts'
+tmp_directory = '../results/tmp'  # For temp files to be zipped
 
-log_per = 3
+start_file_num = 1  # index starts with 1
+save_num = 500
 
 ## Regex for tokenizing words
 token_re = re.compile('([0-9]+[.][0-9]+|[0-9A-Za-zㄱ-ㅎㅏ-ㅢ가-힣]+|\.\.+|\.|\?|\!|\,)')
 in_brackets_re = re.compile('\(.*?\)')  # Limitation on nested brackets like '(a(b)c)'
 
-## Constants
 
 
 ## Functions
@@ -31,38 +32,48 @@ def tokenize_sentence(s):
 
 
 ## Main
-
+print('Start tokenizing!')
 with zipfile.ZipFile(read_zip_file, 'r') as z:
   print("Open script ZIP file")
-  zip_info = z.infolist()
 
-  for idx, zi in enumerate(zip_info):
-    fn = zi.filename
-    if (not fn.endswith('.json')):
-      continue
+  for idx, fn in enumerate(sorted(z.namelist()), 1):
+    if (idx >= start_file_num and fn.endswith('.json')):
+      write_path = join(tmp_directory, basename(fn))
+      result_json = []
 
-    if (idx + 1) % log_per == 0:
-      print("Parsing file %6d: %s" % (idx+1, fn))
-    write_path = join(write_directory, basename(fn))
-    result_json = []
+      with z.open(fn, 'r') as readfile:
+        try:
+          dialogs = json.load(readfile)
+          result = []
+          for d in dialogs:
+            result_dialog = {'context': d['context']}
+            result_dialog['lines'] = []
+            for l in d['lines']:
+              result_line = {'speaker': l['speaker'], 'message': l['message']}
+              result_line['tokens'] = tokenize_sentence(l['message'])
+              result_dialog['lines'].append(result_line)
+            result.append(result_dialog)
+          if len(result) > 0:
+            with open(write_path, 'w', encoding='utf8') as writefile:
+              json.dump(result, writefile, ensure_ascii=False)
+        except:
+          print('JSON is Empty!')
 
-    with z.open(fn, 'r') as readfile:
-      with open(write_path, 'w', encoding='utf8') as writefile:
-        dialogs = json.load(readfile)
-        result = []
-        for d in dialogs:
-          result_dialog = {'context': d['context']}
-          result_dialog['lines'] = []
-          for l in d['lines']:
-            result_line = {'speaker': l['speaker'], 'message': l['message']}
-            result_line['tokens'] = tokenize_sentence(l['message'])
-            result_dialog['lines'].append(result_line)
-          result.append(result_dialog)
-        json.dump(result, writefile, ensure_ascii=False)
+    if (idx % save_num == 0):
+      tmp_files = listdir(tmp_directory)
+      for f in tmp_files:
+        shutil.move(join(tmp_directory, f), join(write_directory, f))
+      print("Files are moved from tmp to dest folder; step %5d" % idx)
 
-with zipfile.ZipFile(write_zip_file, 'w') as z:
-  file_names = [f for f in listdir(write_directory) if (isfile(join(write_directory, f)) and f.endswith('.json'))]
-  for f in file_names:
-    f_path = join(write_directory, f)
-    z.write(f_path, arcname=f)
-    remove(f_path)
+  for f in listdir(tmp_directory):
+    tmp_files = listdir(tmp_directory)
+    for f in tmp_files:
+      shutil.move(join(tmp_directory, f), join(write_directory, f))
+    print("Move the last files from tmp to dest folder!")
+
+# with zipfile.ZipFile(write_zip_file, 'w') as z:
+#   file_names = [f for f in listdir(write_directory) if (isfile(join(write_directory, f)) and f.endswith('.json'))]
+#   for f in file_names:
+#     f_path = join(write_directory, f)
+#     z.write(f_path, arcname=f)
+#     remove(f_path)
