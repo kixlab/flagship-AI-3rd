70f0800f64fdb147ea7b1c7c635b0ba6bc8e7934
Seongho Park <bluetosh7@gmail.com>
2018-07-11 17:18:56 +0900

    feat: add virturalenv & version up trainer_gensim (v0.1.1)

---
+++ bin/word2vec/_uu.py
@@ -0,0 +1,5 @@
+# Trick for import module in parent directory
+
+import sys
+sys.path.append("../")
+from universal_utils import *
--- bin/word2vec/trainer_gensim.py
+++ bin/word2vec/trainer_gensim.py
@@ -1,4 +1,20 @@
-# Gensim Trainer v0.1
+# Gensim Trainer v0.1.1
+
+IS_COLAB = False
+
+if IS_COLAB:
+	import universal_utils as uu
+else:
+	import _uu as uu
+
+## Variables
+input_file = '../../results/token-scripts-reduce3.txt'
+output_file = '../../results/gensim-test'
+
+training_epoch = 1000
+max_sentences = 100000
+log_per = 50
+save_per = 200
 
 from gensim.models import Word2Vec
 
@@ -8,10 +24,29 @@ def load_sentence(filename):
                     for l in readfile.readlines()]
 	return lines
 
+uu.print_dt("Load sentences...")
+tokenized_contents = load_sentence(input_file)[:max_sentences]
 
-tokenized_contents = load_sentence('../../results/token-scripts-reduce3.txt')[:100000]
-embedding_model = Word2Vec(tokenized_contents, size=50, window = 2, min_count=5, workers=4, iter=10000, sg=1)
+uu.print_dt("Create Word2Vec model...")
+model = Word2Vec(iter=1, size=50, window=2, min_count=5,
+                 workers=4, sg=1, compute_loss=True)
 
-embedding_model.save('../../results/token-scripts-reduce3-100000-word2vec')
+uu.print_dt("Build Vocab...")
+model.build_vocab(tokenized_contents)
 
-print(embedding_model.most_similar(positive=["나는"], topn=100))
+loss_sum = 0
+for epoch in range(1, training_epoch + 1):
+	model.train(
+		tokenized_contents, total_examples=model.corpus_count, epochs=model.epochs, compute_loss=True)
+	loss_sum += model.get_latest_training_loss()
+
+	if epoch % log_per == 0:
+		uu.print_dt('Loss from %6d to %6d : %f' %(epoch - log_per, epoch, loss_sum / log_per))
+		loss_sum = 0
+	
+	if epoch % save_per == 0:
+		model.save(output_file + '_%d' % epoch)
+		uu.print_dt("Model with %6d iterations is saved." % epoch)
+
+model.save(output_file + '_final')
+uu.print_dt('Final Model is saved!')
