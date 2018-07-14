028fadffcba150c2991e346d693133586f318050
Seongho Park <bluetosh7@gmail.com>
2018-07-14 16:55:17 +0900

    feat: add get_logger and etc funcs to uu

--- bin/test.py
+++ bin/test.py
@@ -3,6 +3,43 @@
 import os
 import csv
 import tqdm
+import logging
+from pytz import timezone, utc
+from datetime import datetime
 
-for i in tqdm.trange(1, 101):
-  print(i)
+
+# logger 인스턴스를 생성 및 로그 레벨 설정
+logger = logging.getLogger('name')
+logger.setLevel(logging.DEBUG)
+
+# fileHandler와 StreamHandler를 생성
+fileHandler = logging.FileHandler('./test.log')
+streamHandler = logging.StreamHandler()
+
+fileHandler.setLevel(logging.DEBUG)
+streamHandler.setLevel(logging.INFO)
+
+log_format = '[%(asctime)s:%(lineno)03s] %(message)s'
+formatter = logging.Formatter(log_format)
+
+def customTime(*args):
+  utc_dt = utc.localize(datetime.utcnow())
+  my_tz = timezone("Asia/Seoul")
+  converted = utc_dt.astimezone(my_tz)
+  return converted.timetuple()
+
+logging.Formatter.converter = customTime
+
+fileHandler.setFormatter(formatter)
+streamHandler.setFormatter(formatter)
+
+# Handler를 logging에 추가
+logger.addHandler(fileHandler)
+logger.addHandler(streamHandler)
+
+# logging 
+logger.debug("debug")
+logger.info("info")
+logger.warning("warning")
+logger.error("error")
+logger.critical("critical")
--- bin/universal_utils.py
+++ bin/universal_utils.py
@@ -1,11 +1,13 @@
 import time
-import datetime
-import pytz
+import os
+import logging
+from pytz import timezone, utc
+from datetime import datetime
 
 def get_current_datetime():
-	local = pytz.timezone("Asia/Seoul")
+	local = timezone("Asia/Seoul")
 	ts = time.time()
-	return datetime.datetime.fromtimestamp(ts, local).strftime('%Y-%m-%d %H:%M:%S')
+	return datetime.fromtimestamp(ts, local).strftime('%Y-%m-%d %H:%M:%S')
 
 def print_dt(s):
 	print('[%s] ' % get_current_datetime() + s)
@@ -16,3 +18,42 @@ def load_text_file(filename):
 		lines = [l.decode('utf8', 'ignore').strip()
 								 for l in readfile.readlines()]
 	return lines
+
+def get_base_path():
+	bin_path = os.path.dirname( os.path.abspath( __file__ ) )
+	base_path = os.path.abspath(os.path.join(bin_path, os.pardir))
+	return base_path
+
+# Console: INFO ~
+# Log file: DEBUG ~
+def get_custom_logger(name, log_path):
+	# logger 인스턴스를 생성 및 로그 레벨 설정
+	logger = logging.getLogger(name)
+	logger.setLevel(logging.DEBUG)
+
+	# fileHandler와 StreamHandler를 생성
+	fileHandler = logging.FileHandler(log_path)
+	streamHandler = logging.StreamHandler()
+
+	fileHandler.setLevel(logging.DEBUG)
+	streamHandler.setLevel(logging.INFO)
+
+	log_format = '[%(asctime)s:%(lineno)03s] %(message)s'
+	formatter = logging.Formatter(log_format)
+
+	def customTime(*args):
+		utc_dt = utc.localize(datetime.utcnow())
+		my_tz = timezone("Asia/Seoul")
+		converted = utc_dt.astimezone(my_tz)
+		return converted.timetuple()
+
+	logging.Formatter.converter = customTime
+
+	fileHandler.setFormatter(formatter)
+	streamHandler.setFormatter(formatter)
+
+	# Handler를 logging에 추가
+	logger.addHandler(fileHandler)
+	logger.addHandler(streamHandler)
+
+	return logger
